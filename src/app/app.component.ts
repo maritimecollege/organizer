5e6d16cf681b0c9a67f60f3f7e767d3a475524d7
Ilya Marnopolskiy <i.marnopolskiy@solarlab.ru>
2023-06-06 18:04:06 +0300

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import * as Parse from 'parse';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'organizer';
+
+  constructor() {
+    // Initialize your Parse App Keys
+    Parse.initialize("eVIEh9KlNVzj2BNGqbRhRSnKsoLWhm9lc3SeJMqe","aor6mfB49UFJtbQCsns2toOCQzbGCbjDLX17kTnu"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+    (Parse as any).serverURL = 'https://parseapi.back4app.com/'
+  }
+
+  saveNewPlayer() {
+    //Create your Parse Object
+    const soccerPlayer = new Parse.Object('SoccerPlayer');
+    //Define its attributes
+    soccerPlayer.set('playerName', 'A. Wed');
+    soccerPlayer.set('yearOfBirth', 1997);
+    soccerPlayer.set('emailContact', 'a.wed@email.io');
+    soccerPlayer.set('attributes', ['fast', 'good conditioning']);
+    try {
+      //Save the Object
+      const result = soccerPlayer.save() as any;
+      alert('New object created with objectId: ' + result.id);
+    } catch (error: any) {
+      alert('Failed to create new object: ' + error?.message);
+    }
+  }
+
+}
---
+++ src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { DateService } from 'src/app/shared/date.service';
+import { Week } from '../../models/week.model';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
+})
+export class CalendarComponent implements OnInit {
+
+
+  calendar: Week[] = [];
+
+  constructor(private dateService: DateService) { }
+
+  ngOnInit(): void {
+    this.dateService.date.subscribe(this.generate.bind(this))
+  }
+
+  generate(now: moment.Moment) {
+    const startDay = now.clone().startOf('month').startOf('week');
+    const endDay = now.clone().endOf('month').endOf('week');
+
+    const date = startDay.clone().subtract(1, 'day');
+
+    const calendar = []
+
+    while (date.isBefore(endDay, 'day')) {
+      calendar.push({
+        days: Array(7)
+        .fill(0)
+        .map(() => {
+          const value = date.add(1, 'day').clone();
+          const active = moment().isSame(value, 'date');
+          const disabled = !now.isSame(value, 'month');
+          const selected = now.isSame(value, 'date') && moment().isSame(value, 'month');
+
+
+          return {
+            value, active, disabled, selected
+          }
+        })
+      })
+    }
+
+    this.calendar = calendar;
+
+  }
+
+  select(day: moment.Moment) {
+    this.dateService.changeDate(day);
+  }
+
+}
